3ff3fe60cd39b3c91bdbbd921f27e53990bc4705
Wonseo Huh <whuh@alliancepharmaco.com>
2019-10-25 13:51:12 -0400

    initial commit

--- myfirstreact/src/App.js
+++ myfirstreact/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
@@ -9,23 +9,86 @@ import Person from './Person/Person';
 //     </div>
 //   );
 // }
-
 /**
  * className ="csscode"
  * export default classname. React uses in JSX in javascript file.  vs in Angular, angular uses class='csscode ' in html.
  * has have to be in one root element(ex. <div>
  * */
-let App = () => {
-    return (
-        <div className="App">
-            <h1>this is React App</h1>
-            <p>this is wokring too</p>
-            <Person name={"Max"} age={"28"}/>
-            <Person name={"Won"} age={"27"}> My Hobbies: Racing</Person>
-            <Person name={"jojo"} age={"1"}/>
-            <Person/>
-        </div>
-    );
-};
+
+/** probs vs state
+ * state is feature from Component in React.*/
+class App extends Component {
+    state = {
+        persons: [
+            { name: 'Max', age:28 },
+            { name: 'Won', age:99 },
+            { name: 'Jojo', age:999 },
+        ],
+        otherStates: 'some other value'
+
+    }
+
+    switchNameHandler  = (newName) => {
+        //console.log('was clicked');
+        //DONT DO THIS this.state.persons[0].name = 'WONSEO';
+        //otherStates is not going to be changed.
+        this.setState({
+            persons: [
+                { name: newName, age:28 },
+                { name: 'WonSEO', age:27 },
+                { name: 'Jorno Joski', age:1 },
+            ]
+        })
+    }
+
+    nameChangeHandler = (event) => {
+        this.setState({
+            persons: [
+                { name: 'Max', age:11111 },
+                { name: event.target.value, age:100 },
+                { name: 'Jorno Joski', age:1000 },
+            ]
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>this is React App</h1>
+                <p>this is wokring too</p>
+                <button onClick={() => this.switchNameHandler('JOJOSKI')}>Switch Name</button>
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age}/>
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    click={this.switchNameHandler.bind(this,'Ao Xiang')}> My Hobbies: Racing</Person>
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age}/>
+                <Person/>
+            </div>
+        );
+    }
+
+}
+// let App = () => {
+//     return (
+//         <div className="App">
+//             <h1>this is React App</h1>
+//             <p>this is wokring too</p>
+//             <button>Switch Name</button>
+//             <Person name={"Max"} age={"28"}/>
+//             <Person name={"Won"} age={"27"}> My Hobbies: Racing</Person>
+//             <Person name={"jojo"} age={"1"}/>
+//             <Person/>
+//         </div>
+//     );
+// }
 
 export default App;
+
+
+/**angular =chen
+ * chris =*/
--- myfirstreact/src/Person/Person.js
+++ myfirstreact/src/Person/Person.js
@@ -10,8 +10,10 @@ import React from 'react';
 const person = (props) => {
     return (
         <div>
-            <p>I'm a {props.name} and I am {props.age}</p>
+            <p onClick={props.click}>I'm a {props.name} and I am {props.age}</p>
             <p>{props.children}</p>
+            /* listen to onchanges. double binding.*/
+            <input type="text" onChange={}/>
         </div>
         )
 };
